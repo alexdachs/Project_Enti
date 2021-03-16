915348276c95787ffdbcb4dc10341e9a5be9409f
Alex Dachs Manchon <alex.dachs34@gmail.com>
2021-03-16 20:03:53 +0100

    Script tiempo
    Script para el contador de tiempo

---
+++ Assets/Scrips/StopWatch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopWatch : MonoBehaviour
+{
+    float timer;
+    float seconds;
+    float minutes;
+    float hours;
+
+    [SerializeField] Text stopWatchText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stopWatchCalcul ();
+    }
+    void stopWatchCalcul() {
+        timer += Time.deltaTime;
+        seconds = timer % 60;
+        minutes = timer / 60;
+        hours = timer / 3600;
+
+        stopWatchText.text = hours + ":" + minutes + ":" + seconds;
+
+
+    }
+}
