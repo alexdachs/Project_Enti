7368520ee5fee0b21cd071ec29e087a0d3dd6faf
Alex Dachs Manchon <alex.dachs34@gmail.com>
2021-04-05 22:07:49 +0200

    Menu
    Menu

--- Assets/botones.cs
+++ Assets/botones.cs
@@ -14,4 +14,30 @@ public class botones : MonoBehaviour
     public void siguientenivel () {
         SceneManager.LoadScene("Level_1");
     }
+    //Controles menu
+    public void play () {
+        SceneManager.LoadScene("Levels");
+    }
+    public void options () {
+        SceneManager.LoadScene("Options");
+    }
+    public void credits () {
+        SceneManager.LoadScene("Credits");
+    }
+    public void quit () {
+        Application.Quit();
+    }
+    //Selector niveles
+    public void lvl1 () {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void lvl2 () {
+        SceneManager.LoadScene("Level_1");
+    }
+    public void lvl3 () {
+        SceneManager.LoadScene("Level_2");
+    }
+    public void lvl4 () {
+        SceneManager.LoadScene("Level_3");
+    }
 }
