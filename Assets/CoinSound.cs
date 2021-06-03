9b0f6c860523a30e63f43510746c7457566c0efd
Pau Gámez <pau.gamez.pallares@enti.cat>
2021-06-03 16:18:38 +0200

    Sonido moneda

---
+++ Assets/CoinSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSound : MonoBehaviour
+{
+    public character player;
+    private AudioSource coin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coin = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.coin == true)
+        {
+            coin.Play();
+        }
+    }
+}
